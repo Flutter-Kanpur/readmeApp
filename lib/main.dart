0fe3e3f6cd21437dc7d3a126f7da175c4e806bd2
Karan <karanjain2527@gmail.com>
2026-01-19 13:59:15 +0530

    Gradient Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:readme_blogapp/features/home_page/home_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:readme_blogapp/shared/widgets/gradient_background.dart';
+import 'package:readme_blogapp/shared/widgets/gradient_button.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url:'https://uktnmjykbyuvfsbtawwg.supabase.co',
-    anonKey:'sb_publishable_p3fuhjG-r3Iu2AA0Ayak2w_9Ms57rb5',
+    url: 'https://uktnmjykbyuvfsbtawwg.supabase.co',
+    anonKey: 'sb_publishable_p3fuhjG-r3Iu2AA0Ayak2w_9Ms57rb5',
   );
   runApp(const MyApp());
 }
@@ -17,24 +17,33 @@ Future<void> main() async{
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(375, 812),
-        minTextAdapt: true,
-        builder: (context, child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Flutter Demo',
-            theme: ThemeData(
-              textTheme: GoogleFonts.poppinsTextTheme(),
-              colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            textTheme: GoogleFonts.poppinsTextTheme(),
+            colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+          ),
+          home: GradientBackground(
+            child: Center(
+              child: Custom3DButton(
+                width: 280,
+                height: 56,
+                text: "Create Account",
+                onTap: () {
+                  print("Pressed");
+                },
+              ),
             ),
-            home: const HomeScreen(),
-          );
-        },
+          ),
+        );
+      },
     );
   }
 }
-
---
+++ lib/shared/widgets/gradient_button.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class Custom3DButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+  final double height;
+  final double width;
+
+  const Custom3DButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+    this.height = double.infinity,
+    this.width = double.infinity,
+  });
+
+  //To use this just give your custom Height, Width, Text and onTap
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(height / 2),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.4),
+              blurRadius: 12,
+              offset: const Offset(0, 6),
+            ),
+          ],
+        ),
+        child: Stack(
+          children: [
+            /// Base dark gradient
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(height / 2),
+                gradient: const LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment(0, 9),
+                  stops: [0.0, 0.05],
+                  colors: [Color.fromARGB(255, 25, 25, 25), Colors.black],
+                ),
+              ),
+            ),
+
+            //top left corner ka gradient
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(height / 2),
+                gradient: LinearGradient(
+                  begin: Alignment(-1, -1),
+                  end: Alignment(0, 1),
+                  stops: [-1, 0.2],
+                  colors: [Colors.white.withOpacity(0.35), Colors.transparent],
+                ),
+              ),
+            ),
+
+            //top center ka gradient
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(height / 2),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.center,
+
+                  colors: [Colors.white.withOpacity(0.28), Colors.transparent],
+                ),
+              ),
+            ),
+
+            //top right corner ka gradient
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(height / 2),
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomCenter,
+                  stops: [-1, 0.2],
+
+                  colors: [Colors.white.withOpacity(0.25), Colors.transparent],
+                ),
+              ),
+            ),
+
+            /// Text
+            Center(
+              child: Text(
+                text,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 0.4,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
