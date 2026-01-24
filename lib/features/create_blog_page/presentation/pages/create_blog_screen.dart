cfc53887f054148d503b89ebcffaad6a65262e24
Yesra Fatima <147398141+yesra29@users.noreply.github.com>
2026-01-24 15:18:28 +0530

    #COMMENT architecture setup for write blog feature

---
+++ lib/features/create_blog_page/presentation/pages/create_blog_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateBlogScreen extends StatefulWidget {
+  const CreateBlogScreen({super.key});
+
+  @override
+  State<CreateBlogScreen> createState() => _CreateBlogScreenState();
+}
+
+class _CreateBlogScreenState extends State<CreateBlogScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
